dcd0fdbb3de83ff241bb5c95aaf204dbfac1c4fb
Marvelous Obiji <marvelous@gmail.com>
2022-10-29 05:05:20 +0100

    Updated the models.py file in the musicapp

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,32 @@
+from email.policy import default
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=250)
+    date_released = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
